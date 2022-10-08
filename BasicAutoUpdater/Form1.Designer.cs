2633463314f65c76ef2ece2c4970c44367d5e296
Raymond <34816356+Gurnski@users.noreply.github.com>
2022-10-09 00:47:53 +0100

    Upload of full project

---
+++ BasicAutoUpdater/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BasicAutoUpdater
+{
+    partial class autoUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clientVersionLabel = new System.Windows.Forms.Label();
+            this.serverVersionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clientVersionLabel
+            // 
+            this.clientVersionLabel.AutoSize = true;
+            this.clientVersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientVersionLabel.Location = new System.Drawing.Point(265, 62);
+            this.clientVersionLabel.Name = "clientVersionLabel";
+            this.clientVersionLabel.Size = new System.Drawing.Size(200, 32);
+            this.clientVersionLabel.TabIndex = 0;
+            this.clientVersionLabel.Text = "Client Version:";
+            // 
+            // serverVersionLabel
+            // 
+            this.serverVersionLabel.AutoSize = true;
+            this.serverVersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverVersionLabel.Location = new System.Drawing.Point(265, 139);
+            this.serverVersionLabel.Name = "serverVersionLabel";
+            this.serverVersionLabel.Size = new System.Drawing.Size(209, 32);
+            this.serverVersionLabel.TabIndex = 1;
+            this.serverVersionLabel.Text = "Server Version:";
+            // 
+            // autoUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.serverVersionLabel);
+            this.Controls.Add(this.clientVersionLabel);
+            this.Name = "autoUpdateForm";
+            this.Text = "Basic Autoupdater example";
+            this.Load += new System.EventHandler(this.autoUpdateForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clientVersionLabel;
+        private System.Windows.Forms.Label serverVersionLabel;
+    }
+}
+
---
+++ BasicAutoUpdater/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicAutoUpdater
+{
+    public partial class autoUpdateForm : Form
+    {
+        string serverVersion; //Create serverVersion variable
+        string clientVersion = "1.0"; //Create clientVersion variable and define the version of this client
+        string serverLink = "https://danthedev.co.uk/api/autoUpdate/test/"; //Link of raw text containing server (updated client) version.
+        WebClient client = new WebClient(); //Define webclient object
+
+        public autoUpdateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void updateClient()
+        {
+            clientVersionLabel.Text = "Client Version: " + clientVersion; //Updates the label with the version
+            serverVersion = client.DownloadString(serverLink); //Downloads the server-side client version
+            serverVersionLabel.Text = "Server Version: " + serverVersion; //Updates the label with the version
+
+            if (serverVersion != clientVersion) //If the server version isn't equal to the client version
+            {
+                DialogResult messageBox = MessageBox.Show("A new version is available. Download?", "Update", MessageBoxButtons.YesNo); //Displays a message box with the title "Update" and two buttons (Yes/No)
+
+                if (messageBox == DialogResult.Yes) //If user presses the "Yes" button on the messagebox
+                {
+                    client.DownloadFile("File link", "File name"); //Function will download the new client via the link and save it as the name provided
+                }
+            }
+        }
+
+        private void autoUpdateForm_Load(object sender, EventArgs e) //Fires when the form is loaded
+        {
+            updateClient(); //Calls the updateclient function as defined above
+        }
+    }
+}
